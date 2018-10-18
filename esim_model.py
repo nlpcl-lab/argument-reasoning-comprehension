37b480facc07f19436daec9132727cac96c26fa3
ddehun <ddehun@kaist.ac.kr>
2018-10-18 19:14:07 +0900

    model init

--- esim_model.py
+++ esim_model.py
@@ -4,8 +4,16 @@ ESIM model for sentence pair embedding
 '''
 
 class ESIM():
-    def __init(self):
-        pass
+    def __init(self, embed_matrix):
+        self.word_embedding = tf.Variable(embed_matrix, trainable=False, dtype=tf.float32, name='word_embedding')
+        self.global_step = tf.Variable(0, trainable=False, name='global_step')
+
+        # Dropout rate
+        self.rnn_keeprate = tf.placeholder_with_default(1.0, shape=(), name='rnn_keep_rate')
+        self.fcn_keeprate = tf.placeholder_with_default(1.0, shape=(), name='fcn_keep_rate')
+
+        self._build_model()
+        self.saver = tf.train.Saver(tf.global_variables())
 
     def _build_model(self):
         pass
