eee551739175f00687dc6fc3119dcf547548e0c6
ddehun <ddehun@kaist.ac.kr>
2018-10-18 19:26:07 +0900

    input encoding(ing)

--- esim_model.py
+++ esim_model.py
@@ -16,12 +16,24 @@ class ESIM():
         self.saver = tf.train.Saver(tf.global_variables())
 
     def _build_model(self):
-        hyp = tf.placeholder(tf.int64, [None, None], name='hyp_input')
-        pre = tf.placeholder(tf.int64, [None, None], name='pre_input')
+        pre = tf.placeholder(tf.int64, [None, None], name='hyp_input')
+        hyp = tf.placeholder(tf.int64, [None, None], name='pre_input')
         label = tf.placeholder(tf.int64,[None,3],name='label')
 
-    def _input_encoding(self):
-        pass
+        pre_state, hyp_state = self._input_encoding(pre,hyp,label)
+
+    def _input_encoding(self,pre,hyp,label):
+        pre_enc_fw, pre_enc_bw, hyp_enc_fw, hyp_enc_bw = self._build_cells(self.rnn_keeprate)
+
+        pre_outputs, pre_states = tf.nn.bidirectional_dynamic_rnn(pre_enc_fw, pre_enc_bw,
+                                                                  tf.nn.embedding_lookup(self.word_embedding, pre),
+                                                                  dtype=tf.float32)
+        hyp_outputs, hyp_states = tf.nn.bidirectional_dynamic_rnn(hyp_enc_fw, hyp_enc_bw,
+                                                                  tf.nn.embedding_lookup(self.word_embedding, hyp),
+                                                                  dtype=tf.float32)
+
+
+
 
     def _local_inference(self):
         pass
