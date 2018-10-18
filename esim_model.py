81a2b8bc67320cf1c46b60f85236eea235f76351
ddehun <ddehun@kaist.ac.kr>
2018-10-18 19:13:23 +0900

    esim model function

--- esim_model.py
+++ esim_model.py
@@ -7,4 +7,34 @@ class ESIM():
     def __init(self):
         pass
 
+    def _build_model(self):
+        pass
+
+    def _input_encoding(self):
+        pass
+
+    def _local_inference(self):
+        pass
+
+    def _inference_composition(self):
+        pass
+
+    def _build_op(self):
+        pass
+
+    def _build_cells(self):
+        pass
+
+    def _cell(self):
+        pass
+
+    def train(self):
+        pass
+
+    def test(self):
+        pass
+
+    def write_summary(self):
+        pass
+
     
